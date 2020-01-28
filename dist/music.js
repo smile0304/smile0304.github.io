6df8dea1baa984dedcb2b18b4798cc5326d73cba
smile0304 <smile@smilehacker.net>
2020-01-28 13:23:01 +0800

    Site updated: 2020-01-28 13:23:00

--- dist/music.js
+++ dist/music.js
@@ -3,29 +3,11 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-	{
-        name: "canon in d",
-        artist: 'Brian Crain',
-        url: 'https://yfzhou.oss-cn-beijing.aliyuncs.com/blog/music/Brian%20Crain%20-%20canon%20in%20d.mp3',
-        cover: 'http://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
-      },
-	  {
-        name: "Apologize",
-        artist: 'Martin Ermen',
-        url: 'https://yfzhou.oss-cn-beijing.aliyuncs.com/blog/music/Martin%20Ermen%20-%20Apologize.mp3',
-        cover: 'http://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
-      },
-	  {
-        name: "River Flows in You",
-        artist: 'Yiruma',
-        url: 'https://yfzhou.oss-cn-beijing.aliyuncs.com/blog/music/Yiruma%20-%20River%20Flows%20in%20You.flac',
-        cover: 'http://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
-      },
       {
-        name: '惊蛰',
-        artist: '音阙诗听/王梓钰',
-        url: 'http://www.ytmp3.cn/down/48755.mp3',
-        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+        name: "山鬼（陶笛钢琴合奏版）（Cover winky诗）",
+        artist: '笛呆子囚牛 / 昼夜',
+        url: 'http://blog-pwntools.oss-cn-beijing.aliyuncs.com/%E8%83%8C%E6%99%AF%E9%9F%B3%E4%B9%90/%E7%AC%9B%E5%91%86%E5%AD%90%E5%9B%9A%E7%89%9B,%E6%98%BC%E5%A4%9C-%E5%B1%B1%E9%AC%BC.mp3',
+        cover: 'http://blog-pwntools.oss-cn-beijing.aliyuncs.com/%E8%83%8C%E6%99%AF%E9%9F%B3%E4%B9%90/109951162809123680.jpg!blog_',
       }
     ]
 });
